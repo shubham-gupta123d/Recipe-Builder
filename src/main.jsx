7bff090bcce8242dc12534dc22b52d8be323e871
Shubham <shubhamgupta7667@gmail.com>
2025-05-21 17:26:46 +0530

    Fixed router issues and updated for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename="/Recipe-Builder">
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
